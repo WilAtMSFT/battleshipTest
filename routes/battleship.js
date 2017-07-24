6de8376472e41657e5d78c6f1ad28dd37c7d9471
Wil At Msft <WilAtMsft@outlook.com>
2017-07-24 11:15:59 -0400

    Add a place for user to enter firing coordinates

--- routes/battleship.js
+++ routes/battleship.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET battleship page. */
 router.get('/', function(req, res, next) {
   res.render('battleship', { title: 'Battleship!' });
+});
+
+/* POST to battleship page. */
+router.post('/', function(req, res) {
+
+    // Get our form values. 
+    let x = req.body.xcoordinate;
+    let y = req.body.ycoordinate;
+    let result = 'Miss';
+
+    res.render('battleship', { title: 'Battleship!', xcoordinate: x, ycoordinate: y, result: result});
 });
 
 module.exports = router;
